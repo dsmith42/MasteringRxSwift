33f443f5e2c28bfe7129e319fc8d18cd1e2653f5
Dan Smith <dsmith42@tutanota.com>
2023-01-25 12:12:16 +0000

    Add an observable to ViewController

--- HelloRxSwift/HelloRxSwift/ViewController.swift
+++ HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
 
+		_ = Observable.from(optional: [1, 2, 3, 4, 5])
+	}
 
 }
 
