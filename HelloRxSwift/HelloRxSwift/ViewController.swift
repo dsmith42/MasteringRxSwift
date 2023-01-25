108627d925ff3542004f35bc16d9e1bbb29fe178
Dan Smith <dsmith42@tutanota.com>
2023-01-25 11:28:38 +0000

    Setup empty HelloRxSwift project

---
+++ HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloRxSwift
+//
+//  Created by Dan Smith on 25/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
