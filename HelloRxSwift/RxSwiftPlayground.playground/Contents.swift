4d5cb02c45493c431be83524ae7aeb962b186a1d
Dan Smith <dsmith42@tutanota.com>
2023-01-25 12:51:51 +0000

    Creating an observable using the create function

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -3,7 +3,18 @@ import RxSwift
 let disposeBag = DisposeBag()
 
 Observable.of("A", "B", "C")
-	.subscribe(onNext: {
+	.subscribe {
 		print($0)
-	})
+	}
 	.disposed(by: disposeBag)
+
+Observable<String>.create { observer in
+	observer.onNext("A")
+	observer.onCompleted()
+	observer.onNext("?")
+	return Disposables.create()
+}.subscribe(onNext: { print($0) },
+						onError: { print($0) },
+						onCompleted: { print("Completed") },
+						onDisposed: { print("Disposed") })
+.disposed(by: disposeBag)
